4ea8e40f365ee2690692207c5a165030ccdf97cf
Cheah Tuck Feng <feng7005@gmail.com>
2024-04-04 01:25:55 +0800

    Modify function getComputerChoice() and add function getPlayerChoice()  into index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let computer = Math.floor(Math.random() * 3);
+    
+    if (computer == 0)  computer = "rock";
+    else if (computer == 1)  computer = "paper";
+    else if (computer == 2)  computer = "scissors";
+
+    return computer;
+}
+ 
+function getPlayerChoice() {
+    let player = prompt("Tell me your choice! Choose between rock, paper, and scissors!");
+    player = player.toLowerCase();
+    
+    return player;
 }
